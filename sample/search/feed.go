145e4bf9d40160b6f7e3282448edd480c37bef19
xuejiawei <xuejiawei@dushu.io>
2020-04-17 18:35:32 +0800

    初始化匹配器

--- sample/search/feed.go
+++ sample/search/feed.go
@@ -25,9 +25,10 @@ func RetrieveFeeds() ([]*Feed, error) {
 	// defer确保，该函数一定会被调用，哪怕函数意外崩溃终止，也能保证关键字defer安排调用的函数会被执行
 	defer file.Close()
 
+	// 当前值为nil的切片，这个切片包含一组指向Feed类型值的指针
 	var feeds []*Feed
 	// 将文件解码到切片里
-	// 这个切片的每一项是一个指向一个Feed类型值的指针
+	// 这个切片的每一项是一个指向一个Feed类型值的指针，传入切片的地址
 	err = json.NewDecoder(file).Decode(&feeds)
 
 	// 不需要检查错误，调用者会做这件事
--- sample/search/match.go
+++ sample/search/match.go
@@ -1,5 +1,13 @@
 package search
 
-func main() {
+// Result保存搜索的结果，声明结构类型
+type Result struct {
+	Field   string
+	Content string
+}
 
+// 声明接口类型
+type Matcher interface {
+	// 参数为Feed类型的指针
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
 }
